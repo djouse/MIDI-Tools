cddabae4015f3c755afb37d23989e0c6e2490143
Jose Dias <jose.d.pereira@inesctec.pt>
2024-05-15 17:05:31 +0100

    Added utility functions to convert midi to array data

--- src/midi/bpm.py
+++ src/midi/bpm.py
@@ -63,18 +63,64 @@ class Bpm:
         return multitrack
     
 
-pretty_midi.PrettyMIDI("input/input.mid")
+pm = pretty_midi.PrettyMIDI("input/input.mid")
+end_time = pm.get_end_time()
+print(end_time)
 
-pp = pypianoroll.from_pretty_midi(pretty_midi.PrettyMIDI("input/input.mid"))
-print(pp)
+cello_c_chord = pretty_midi.PrettyMIDI()
+# Create an Instrument instance for a cello instrument
+cello_program = pretty_midi.instrument_name_to_program('Cello')
+cello = pretty_midi.Instrument(program=cello_program)
+# Iterate over note names, which will be converted to note number later
+for note_name in ['C5', 'E5', 'G5']:
+    # Retrieve the MIDI note number for this note name
+    note_number = pretty_midi.note_name_to_number(note_name)
+    # Create a Note instance for this note, starting at 0s and ending at .5s
+    note = pretty_midi.Note(velocity=100, pitch=note_number, start=0, end=.5)
+    # Add it to our cello instrument
+    cello.notes.append(note)
+# Add the cello instrument to the PrettyMIDI object
+cello_c_chord.instruments.append(cello)
+# Write out the MIDI data
+cello_c_chord.write('cello-C-chord.mid')
+
+fs = FluidSynth()
+fs.play_midi('cello-C-chord.mid')
+metronome = pretty_midi.PrettyMIDI()
+# Create an Instrument instance for a cello instrument
+note = pretty_midi.drum_name_to_note_number('Closed Hi Hat')
+print(note)
+
+
+drum = pretty_midi.Instrument(program=0, is_drum=True, name='Drums')
+# Iterate over note names, which will be converted to note number later
+# Retrieve the MIDI note number for this note name
+# Create a Note instance for this note, starting at 0s and ending at .5s
+
+current_t=0.0
+t_shot = 60.0/120.0
+time_array = np.arange(current_t,3.0,t_shot)
+print(time_array)
+notes = []
+
+#for t in time_array:
+t=2
+pretty_midi.Note(velocity=100, pitch=note, start=t, end=(t+0.5))
+
+# Add it to our cello instrument
+print(notes)
+drum.notes.append(pretty_midi.Note(velocity=100, pitch=note, start=t, end=(t+0.5)))
+metronome.instruments.append(pretty_midi.Instrument(drum))
+
+# Write out the MIDI data
+metronome.write('cello-C-chord.mid')
+
+#pp = pypianoroll.from_pretty_midi(pretty_midi.PrettyMIDI("input/input.mid"))
+#print(pp)
 
 bpm = Bpm(120)
 track = bpm.bpmToMidiTrack()
-midi = pypianoroll.to_pretty_midi(track)
-midi.write("midi_metronome.mid")
+#midi = pypianoroll.to_pretty_midi(track)
+#midi.write("midi_metronome.mid")
 fs = FluidSynth()
-#fs.play_midi('midi_metronome.mid')
-
-
-print(midi)
-print(track)
+fs.play_midi('cello-C-chord.mid')
